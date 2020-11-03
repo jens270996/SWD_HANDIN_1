8f5f3a56e2e399b512ecdd2df367e12bb5d8190d
Joachim <Kroyer.joachim@gmail.com>
2020-11-03 11:47:42 +0100

    added patial implementation  implementation for Game class

--- MementoLib/Game.cs
+++ MementoLib/Game.cs
@@ -9,5 +9,26 @@ namespace MementoLib
         public class GameMemento
         {
         }
+
+        public class GameObject
+        {
+            private List<Player> players;
+
+
+        }
+
+        public class Player
+        {
+            public Player(string mission, int troops, int countries)
+            {
+                Mission = mission;
+                NumberOfTroops = troops;
+                NumberOfCountries = countries;
+            }
+
+            private string Mission;
+            private int NumberOfTroops;
+            private int NumberOfCountries;
+        }
     }
 }
