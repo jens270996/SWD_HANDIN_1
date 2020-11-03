5adda1f1a3daf2ab97c654bb3bd7ef91ea5ea4e5
andreasstove <andreasstove@outlook.dk>
2020-11-03 11:59:47 +0100

    ændret gamementostate

--- MementoLib/GameMementoState.cs
+++ MementoLib/GameMementoState.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MementoLib
 {
-    class GameMementoState
+    public class GameMementoState
     {
         //string _oldState { get; set; }
         //int _player { get; set; }
