fd4b1d42578d32fc129075fe8b3ea9d0a950f48b
andreasstove <andreasstove@outlook.dk>
2020-11-03 11:56:34 +0100

    GameMementoState

--- MementoLib/GameMementoState.cs
+++ MementoLib/GameMementoState.cs
@@ -6,8 +6,13 @@ namespace MementoLib
 {
     class GameMementoState
     {
-        string _oldState { get; set; }
-        int _player { get; set; }
+        //string _oldState { get; set; }
+        //int _player { get; set; }
+
+        //public Game.Player GameObject;
+        
+        public List<Game.Player> gameObjects { get; set; }
+
 
     }
 }
