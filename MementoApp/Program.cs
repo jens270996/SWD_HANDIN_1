0917f3cf7cd7825dd5a8f11b3e203a07bc1beada
andreasstove <andreasstove@outlook.dk>
2020-11-06 09:45:22 +0100

    App

--- MementoApp/Program.cs
+++ MementoApp/Program.cs
@@ -10,8 +10,20 @@ namespace MementoApp
             Console.WriteLine("Hello World!");
             Game game = new Game();
             game.AddPlayer("Andreas", "Vind", 18, 3);
-            game.ShowPlayers();
+            game.AddPlayer("Jens", "Støt Andreas", 27, 7);
+            game.AddPlayer("Joachim", "Slå", 23, 5);
+            Console.WriteLine("Spil efter oprettelse.");
             game.PrintPlayers();
+            MementoCaretaker mementoCaretaker = new MementoCaretaker();
+            mementoCaretaker.AddMemento(game.Save());
+            Console.WriteLine("Der gemmes en memento, så vi kan gå tilbage til dette stadie");
+            Console.WriteLine("Nu ændres spilleren Jens");
+            game.AlterTroops(2, -5);
+            Console.WriteLine("Nu er tropper for jens ændret og der ser sådan her ud:");
+            game.PrintPlayers();
+            mementoCaretaker.AddMemento(game.Save());
+            Console.WriteLine("Nu laves en memento til denne del.");
+            Console.WriteLine("Nu kaldes den første memento tilbage.");
 
         }
     }
